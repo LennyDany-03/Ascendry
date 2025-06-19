68666a2ec58d57628a8257956232295737cfffcb
LennyDany-03 <lennydany3@gmail.com>
2025-06-19 06:23:15 +0530

    ver: 1.4 fix: "use client" By mistake now fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 
 import Navbar from "../components/navbar.jsx"
 import InteractiveBackground from "../components/interactive-background"
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 
 import Navbar from "../../components/navbar.jsx"
 import Footer from "../../components/footer.jsx"
